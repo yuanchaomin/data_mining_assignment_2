d4dd2bf1dfe58351433baabd6d11abc51cfd765e
Chaomin Yuan <lycanfirst@gmail.com>
2017-06-04 17:40:32 +1000

    Still in test

--- Code/Logit.py
+++ Code/Logit.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import KFold
 cwd = os.getcwd()
 train_data_dir = os.pardir + "\data\small\SMALLER_TRAIN.csv"
 test_data_dir = os.pardir + "\data\small\SMALLER_Test.csv"
+whole_data_dir = os.pardir + "\data\covtype.data"
 
 #---------loading data---------#
 data_train = np.genfromtxt(train_data_dir,delimiter=",")
@@ -17,12 +18,12 @@ data_test = np.genfromtxt(test_data_dir, delimiter = ",")
 
 m,n = np.shape(data_train)
 
-X_train = data_train[:,: 11]
+X_train = data_train[:,: (n -1)]
 y_train = data_train[:, n - 1]
-X_test = data_test[:, : 11]
+X_test = data_test[:, : (n - 1)]
 y_test = data_test[:, n - 1]
 
-# #---------training model---------#
+#---------training model---------#
 # logit_cl = linear_model.LogisticRegression()
 # logit_cl.fit(X_train, y_train)
 # y_predict = logit_cl.predict(X_test)
@@ -41,17 +42,17 @@ tf_index = list(kf.split(X_train, y_train))
 logit_tf_cl =  linear_model.LogisticRegression()
 
 for i in range(0,10):
-	tf_X_train = data_train[tf_index[i][0]][:,: 11]
+	tf_X_train = data_train[tf_index[i][0]][:,: (n -1)]
 	tf_y_train = data_train[tf_index[i][0]][:, n - 1]
 
-	tf_X_test = data_train[tf_index[i][1]][:,: 11]
+	tf_X_test = data_train[tf_index[i][1]][:,: (n - 1)]
 	tf_y_test = data_train[tf_index[i][1]][:, n - 1]
 
 	logit_tf_cl.fit(tf_X_train, tf_y_train)
 	tf_y_predict = logit_tf_cl.predict(tf_X_test)
 
 	tf_c_m = confusion_matrix(tf_y_test, tf_y_predict)
-	tf_acc_rate = np.trace(tf_c_m)/(len(tf_X_train) + len(tf_X_test))
+	tf_acc_rate = np.trace(tf_c_m)/len(tf_X_test)
 
 	print(tf_acc_rate)
 
